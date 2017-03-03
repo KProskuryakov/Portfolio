3cea25884aa8a31f94133883c8ed6830e01dbc6c
kproskuryakov <qGW9j6XOtGtgmoMZP360>
2017-03-03 16:56:24 -0500

    Added navbar to the layout

--- routes/lasergame.js
+++ routes/lasergame.js
@@ -1,12 +1,9 @@
-/**
- * Created by kosty on 3/2/2017.
- */
 var express = require('express');
 var router = express.Router();
 
 /* GET lasergame listing. */
 router.get('/', function(req, res, next) {
-    res.render('lasergame')
+    res.render('lasergame', {title: "Laser Game"})
 });
 
 module.exports = router;
